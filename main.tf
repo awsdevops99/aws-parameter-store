c95709cb728a8b643422b88a15f4664dc2a8428f
om <awsdevopsom@gmail.com>
2025-01-05 10:49:36 +0530

    om

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 
-resource "aws_ssm_parameter" "" {
+resource "aws_ssm_parameter" "parameters" {
     count = length(var.parameters)
      name = var.parameters[count.index].name
      type = var.parameters[count.index].type
