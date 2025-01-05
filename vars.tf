8d196898033102a341dfe6558e0e9564a44b0390
om <awsdevopsom@gmail.com>
2025-01-05 20:45:36 +0530

    update

--- vars.tf
+++ vars.tf
@@ -1,6 +1,13 @@
 variable "parameters"{
    default = [
     {name = "dev.rds.username", type="String",value = "admin1" },
+    {name = "dev.backend.DB_HOST", type="String",value = "" },
+   
+
+
+
+
+
     {name = "dev.rds.password", type = "SecureString", value = "ExpenseApp1234" }
    ]
 }
